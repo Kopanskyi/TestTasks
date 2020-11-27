7daa3a3cf45c5f0c9def55e52e0fb6b0b4963901
Taras Kopanskyi <tkopanskyi@gmail.com>
2020-11-27 17:02:55 +0200

    Add project files.

---
+++ TestTasks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rail_Fence_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestTasks/RailFenceCipher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rail_Fence_Cipher
+{
+    static class RailFenceCipher
+    {
+
+    }
+}
